0a61065199db5c073fa3a71306ecb9a37ec3b56c
eXeGeSeR <cosminandone@yahoo.co.uk>
2024-03-10 14:24:10 +0000

    Update helloWorld.py
    The comments were added to the helloWorld.py

--- helloWorld.py
+++ helloWorld.py
@@ -1,7 +1,9 @@
 while True:
     name = input("Hello! What is your name? ")
+    # The name request only alphanumeric characters 
     if name.isalpha():
         break
+    # If there are no alhpanumeric characters an error will be printed
     else:
         print("Error: Please enter only alphabetical characters for your name.")
 
